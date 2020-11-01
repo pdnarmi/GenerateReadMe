bd036e9078c70b24a8d33f8974d932f27dced0da
Pdnarmi <pdnarmi@ncsu.edu>
2020-11-01 14:22:49 -0500

    initial commit

---
+++ ReadMe.md
@@ -0,0 +1,22 @@
+
+        WriteMe
+        ===============
+        Author:Patrick Narmi
+        -------
+        
+        Introduction
+        ------------
+        This application is designed to streamline the readme writing process!
+
+        Install
+        ------
+        npm i inquirer after downloading WriteMe.js
+
+        Operation Instructions
+        ----------------------
+        Simply follow the onscreen prompts and a readme file will automatically be generated!
+
+        Contribution
+        -------------
+        Email me at pdnarmi@ncsu.edu to contribute!
+    
---
+++ WriteMe.js
@@ -0,0 +1,70 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = ()=>{
+    return inquirer.prompt([
+        {
+            name: 'author',
+            message: 'What is the project author(s) name(s)?'
+        },
+        {
+            name: 'name',
+            message: 'What is the project name?'
+        },
+        {
+            name: 'intro',
+            message: 'An introduction or lead on the problem. "Why does someone need this? What purpose does it serve?"'
+        },
+        {
+            name: 'install',
+            message:'What are the install instructions for your project?'
+        },
+        {
+            name: 'operation',
+            message:'What are the instructions for using your project?'
+        },
+        {
+            name: 'contribution',
+            message: 'How may others contribute to this project?'
+        }
+    ]);
+};
+
+const generateMarkDown = answers =>{
+    return `
+        ${answers.name}
+        ===============
+        Author:${answers.author}
+        -------
+        
+        Introduction
+        ------------
+        ${answers.intro}
+
+        Install
+        ------
+        ${answers.install}
+
+        Operation Instructions
+        ----------------------
+        ${answers.operation}
+
+        Contribution
+        -------------
+        ${answers.contribution}
+    `;
+};
+
+promptUser()
+    .then(answers =>{
+        const markDown = generateMarkDown(answers);
+
+        return writeFileAsync('ReadMe.md', markDown);
+    })
+    .then(()=>{
+        console.log('Successfully wrote ReadMe.md!');
+    })
+    .catch(err => console.log(err));
